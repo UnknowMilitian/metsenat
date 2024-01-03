115bb478604eeb33161e6f1311e80fbb3e84d92c
UnknowMilitiant <j.umariy4238@gmail.com>
2024-01-03 22:30:53 +0500

    Completed login, and beginning dashboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,16 @@
 const buttons = document.querySelectorAll("button");
 const dataButtons = document.querySelectorAll("[data-target]");
 const contentSections = document.querySelectorAll('[id^="content"]');
+const contentSectionsTab = document.querySelectorAll('[id^="tab-content"]');
 const inputs = document.querySelectorAll('input[type="text"]');
 const radios = document.querySelectorAll('input[type="radio"]');
 
 const navbarBar = document.querySelector(".bar");
 const mobileContent = document.querySelector(".mobile_content");
+
+navbarBar.addEventListener("click", () => {
+  mobileContent.classList.toggle("active");
+});
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -22,6 +27,7 @@ buttons.forEach((btn) => {
   });
 });
 
+// ! Js Code For Application Form
 dataButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const targetId = button.getAttribute("data-target");
@@ -31,6 +37,10 @@ dataButtons.forEach((button) => {
     });
 
     contentSections.forEach((section) => {
+      section.classList.toggle("hidden", section.id !== targetId);
+    });
+
+    contentSectionsTab.forEach((section) => {
       section.classList.toggle("hidden", section.id !== targetId);
     });
 
@@ -44,11 +54,11 @@ dataButtons.forEach((button) => {
   });
 
   // Adding initial active class to the first button
-  if (button.getAttribute("data-target") === "content1") {
+  if (
+    button.getAttribute("data-target") === "content1" ||
+    button.getAttribute("data-target") === "tab-content-1"
+  ) {
     button.classList.add("active");
   }
 });
-
-navbarBar.addEventListener("click", () => {
-  mobileContent.classList.toggle("active");
-});
+// ! Js Code For Application Form
