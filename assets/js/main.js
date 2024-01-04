caab45a971d116baaa8b1ecb7cc03b3a43828b8b
UnknowMilitiant <j.umariy4238@gmail.com>
2024-01-04 12:00:27 +0500

    Completed dashboard desktop version

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,3 +62,74 @@ dataButtons.forEach((button) => {
   }
 });
 // ! Js Code For Application Form
+
+// Chart JSON
+// Sample data for two lines
+const labels = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const data1 = [
+  10000, 30000, 25000, 40000, 30000, 20000, 10000, 42000, 34000, 65000, 50000,
+  50000,
+];
+const data2 = [
+  20000, 40000, 20000, 45000, 35000, 35000, 10000, 46000, 20000, 55000, 46000,
+  50000,
+];
+
+// Creating the chart
+const ctx = document.getElementById("myChart").getContext("2d");
+const myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: labels,
+    datasets: [
+      {
+        label: "Homiylar",
+        data: data1,
+        borderColor: "rgba(255, 99, 132, 1)", // color for line 1
+        borderWidth: 1,
+        fill: false,
+        tension: 0.5,
+        pointRadius: 0,
+        pointStyle: "circle",
+      },
+      {
+        label: "Talabalar",
+        data: data2,
+        borderColor: "rgba(54, 162, 235, 1)", // color for line 2
+        borderWidth: 1,
+        fill: false,
+        tension: 0.5,
+        pointRadius: 0,
+        pointStyle: "circle",
+      },
+    ],
+  },
+  options: {
+    scales: {
+      x: {
+        ticks: {
+          borderRadius: 10, // Adjust the label border radius for x-axis
+        },
+      },
+      y: {
+        beginAtZero: true,
+        ticks: {
+          borderRadius: 10, // Adjust the label border radius for y-axis
+        },
+      },
+    },
+  },
+});
